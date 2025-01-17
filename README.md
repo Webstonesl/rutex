8fc660020c27a0696770b626b321163583506034
Timo Barnard <timo.j.barnard@gmail.com>
2025-01-17 10:03:14 +0200

    Implemented a UTF-8 reader system.

---
+++ README.md
@@ -0,0 +1,3 @@
+# RuTeX
+
+A Rust implementation of the TeX typesetting system. (Very much a work in progress)
---
+++ src/build_info.rs
@@ -0,0 +1,5 @@
+pub const GIT_HASH: &str = "d3dca54";
+pub const BUILD_DATE: &str = "2025-01-17 07:58:06 UTC";
+pub const GIT_TAG: Option<&str> = None;
+pub const HAS_CHANGES: bool = true;
+pub const VERSION: &str = "0.1.0";
